7212b6e031500cdc7ce2485e05b26e0aeb2a1c27
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-08-01 23:28:58 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,5 +16,10 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end 
+  
+  def waiters
+  meals.map do |meal|
+    meal.waiter == self 
+  end
 
 end
